80bb14a7911cda395d5d544ba709e351a5e18f6c
siddharth shingne <shingnesid@gmail.com>
2026-03-02 13:18:39 +0530

    New commit

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -45,6 +45,15 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
+// Validate ../../../src/app/(auth)/Register/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/Register">> = Specific
+  const handler = {} as typeof import("../../../src/app/(auth)/Register/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../../src/app/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/">> = Specific
