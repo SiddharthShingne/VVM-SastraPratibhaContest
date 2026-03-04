7af2e029f4a50a429a97374906ca55ec7f4fdefb
Siddharth Shingne <shingnesid@gmail.com>
2026-03-04 12:03:41 +0530

    Remove .next from repository and ignore build files

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oZsmQm9wVcw9v93G+WmpwKSeSbnaQnk1oey8wjP1gxQ=\"\n}"
--- .next/dev/static/chunks/src_app_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__0f0ba101._.css",
-  "static/chunks/node_modules_next_f33973cc._.js",
-  "static/chunks/src_components_cf23ae04._.js"
-],
-    source: "dynamic"
-});
