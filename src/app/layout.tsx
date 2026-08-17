40b6f2d4383227b87144cd3b488cc925e33dd27f
Siddharth Shingne <shingnesid@gmail.com>
2026-08-17 14:53:01 +0530

    top commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,31 @@
 import type { Metadata } from "next";
 import { Suspense } from "react";          // ← ADD
 import LoaderWrapper from "@/components/LoadWrapper";
-
-import { Geist,Geist_Mono } from "next/font/google";
+// import localFont from "next/font/local";
+// import { Geist,Geist_Mono } from "next/font/google";
 import MainLayout from "@/components/layout/page";
 
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+// const geistSans = localFont({
+//   src: "./fonts/Geist.woff2",
+//   variable: "--font-geist-sans",
+// });
+
+// const geistMono = localFont({
+//   src: "./fonts/GeistMono.woff2",
+//   variable: "--font-geist-mono",
+// });
 
 export const metadata: Metadata = {
   title: "Vidhyarthi Vigyan Manthan 2026-27",
@@ -30,7 +40,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-scroll-behavior="smooth">
       <body
-        className={`${geistSans.variable} ${geistMono.variable}`}
+        // className={`${geistSans.variable} ${geistMono.variable}`}
       >
         <Suspense>                          {/* ← ADD */}
           <LoaderWrapper />                 {/* ← ADD */}
